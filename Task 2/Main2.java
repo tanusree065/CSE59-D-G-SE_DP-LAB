ed98cac4b19ff934700c43875ac373b7d321534b
Tanusree Saha <tanush22bd@gmail.com>
2026-06-18 17:09:53 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,13 @@
+class NumberChecker {
+   void check(int num){
+      System.out.println((num % 2 ==0)&& (num >10));
 
+   }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        NumberChecker checker = new NumberChecker();
+        checker.check(14);
+
+    }
+}
