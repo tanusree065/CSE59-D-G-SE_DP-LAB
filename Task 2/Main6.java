a7eb776afc8f94da64113f70f0f96b9d1ffbd515
Tanusree Saha <tanush22bd@gmail.com>
2026-06-18 17:14:27 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,18 @@
+class Applet {
+   void launch() {
+      System.out.println("Applet launched");
 
+   }
+}
+class GameApplet extends Applet {
+    void play() {
+       System.out.println("Playing game");
+    }
+}
+  public class Main6 {
+     public static void main(String[] args) {
+         GameApplet game = new GameApplet();
+         game.launch();
+         game.play();
+     }
+  }
