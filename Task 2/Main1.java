79a32de560e5b41c38f2f2d259df811d22dcb94d
Tanusree Saha <tanush22bd@gmail.com>
2026-06-18 16:22:19 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,13 @@
+class ContestStats{
+    int scores[] ={85,90,95};
+    void printScores(){
+        System.out.println("First Score:"+scores[0]);
+    }
+}
+public class Main1{
+    public static void main(String[]args){
+        ContestStats stats = new ContestStats();
+        stats.printScores();
+    }
 
+}
