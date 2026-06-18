ad919815c86c484b73a23b915a204216fca813e4
Tanusree Saha <tanush22bd@gmail.com>
2026-06-18 17:12:36 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,18 @@
+class Printer {
+  void printData(String text){
+      System.out.println("Text:" + text);
+  }
+    void printData(int number) {
+        System.out.println("Number:" + number);
+    }
+}
 
+public class Main5 {
+   public static void main(String[] args) {
+       Printer printer = new Printer();
+       printer.printData("Hello,World!");
+       printer.printData(100);
+   }
+
+
+}
