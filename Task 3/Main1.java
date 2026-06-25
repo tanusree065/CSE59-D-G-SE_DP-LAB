58870fc8b4caf11dd9e146056c559c7f96679c64
Tanusree Saha <tanush22bd@gmail.com>
2026-06-25 16:59:15 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,40 @@
 
+package lab03;
+public class Lab3 {
+    public static void main(String[] args) {
+}
+class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() { 
+        return username; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+}
+class EmailValidator {
+    public static boolean isValid(String email) {
+        if (email != null && email.contains("@")) {
+            return true;
+        }
+        System.out.println("Invalid email format.");
+        return false;
+    }
+}
+class UserRepository {
+    public void saveToDatabase(User user) {
+        if (EmailValidator.isValid(user.getEmail())) { 
+            System.out.println("Connecting to database...");
+            System.out.println("Saving user " + user.getUsername() + " to the user table.");
+        }
+    }
+}
+}
