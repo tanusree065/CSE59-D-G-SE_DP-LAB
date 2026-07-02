5a5e655a150fc50ff77ae7d9effade0864a7fc2b
Tanusree Saha <tanush22bd@gmail.com>
2026-07-02 16:29:53 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,37 @@
+package Lab4;
+interface MessageSender {
+    void sendMessage(String message);
+}
+class EmailSender implements MessageSender {
+    public void sendMessage (String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+class SMSSender implements MessageSender {
+    public void sendMessage (String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+class NotificationService {
+    private final MessageSender messageSender;
+
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+ public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        MessageSender email = new EmailSender();
+        NotificationService emailNotification = new NotificationService(email);
+        emailNotification.alertUser("Hello! Your account has been created.");
+        System.out.println();
+        MessageSender sms = new SMSSender();
+        NotificationService smsNotification = new NotificationService(sms);
+        smsNotification.alertUser("Alert! OTP is 1234.");
+    }
+}
+
 
