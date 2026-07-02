d00d07a8a1d8e82d682fd4ac13864ffac7e4b04d
Tanusree Saha <tanush22bd@gmail.com>
2026-07-02 16:28:11 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,41 @@
+package Lab4;
+interface Printer {
+    void printDocument();
+}
+interface Scanner {
+    void scanDocument();
+}
+interface FaxMachine {
+    void faxDocument();
+}
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine { 
+public void printDocument() { 
+        System.out.println("MultiFunctionCopier: Printing"); 
+    }
+    public void scanDocument() { 
+        System.out.println("MultiFunctionCopier: Scanning"); 
+    }
+    public void faxDocument() { 
+        System.out.println("MultiFunctionCopier: Faxing"); 
+    }
+}
+class BasicPrinter implements Printer {
+    public void printDocument() { 
+        System.out.println("BasicPrinter: Printing"); 
+    }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println(" Testing MultiFunctionCopier");
+        MultiFunctionCopier mfp = new MultiFunctionCopier();
+        mfp.printDocument();
+        mfp.scanDocument();
+        mfp.faxDocument();
+
+        System.out.println("\n Testing BasicPrinter");
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument(); 
+   }
+}  
+
 
